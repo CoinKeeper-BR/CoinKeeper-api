4968daac7901b48f94f4d59bcece245ddcf5f81f
Eduardo <emaiarezende@gmail.com>
2023-12-28 17:25:26 -0300

    Update .gitignore and server code, add ESLint configuration

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { server } from './server/Server';
 
 server.listen(3000, () => {
-    console.log('Server listening on port 3000');
+	console.log('Server listening on port 3000');
 });
--- src/server/Server.ts
+++ src/server/Server.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const server = express();
 
 server.get('/', (req, res) => {
-    res.send('Deu bom!');
+	res.send('Deu bom!');
 });
 
 
